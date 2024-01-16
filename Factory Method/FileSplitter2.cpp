b87e4d3b8d7b1057f6d2242fa21863306243475e
dal-code <qingz7256@163.com>
2024-01-16 15:24:23 +0800

    第一次修改

--- Factory Method/FileSplitter2.cpp
+++ Factory Method/FileSplitter2.cpp
@@ -1,20 +1,47 @@
+//基类，产品
+class ISplitter{
+public:
+    virtual void split() = 0;
+    virtual ~ISplitter(){}
+};
 
+//工厂基类
+class SplitterFactory{
+public:
+    virtual ISplitter* CreateSplitter()=0;
+    virtual ~SplitterFactory(){}
+};
 
-//具体类
+//具体类，继承基类,重载虚函数
 class BinarySplitter : public ISplitter{
+public:
+    void split() override {
+
+    }
     
 };
 
 class TxtSplitter: public ISplitter{
+public:
+    void split() override {
+
+    }
     
 };
 
 class PictureSplitter: public ISplitter{
-    
+public:
+    void split() override {
+
+    }
+
 };
 
 class VideoSplitter: public ISplitter{
-    
+public:
+    void split() override {
+
+    }
 };
 
 //具体工厂
